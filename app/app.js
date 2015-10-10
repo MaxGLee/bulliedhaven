6cad36756a47c89430790db656373acc4c3c87df
CPink <cepinkerton.85@gmail.com>
2015-10-09 23:59:25 -0600

    adds ngRoute fixes shit

--- app/app.js
+++ app/app.js
@@ -1,3 +1,13 @@
-var app = angular.module('bulliedHaven', []);
+var app = angular.module('bulliedHaven', ['ngRoute']);
 
-
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: '/app/views/home.html',
+			controller: 'HomeController'
+		})
+		.when('/login', {
+			templateUrl: '/app/views/login.html',
+			controller: 'LoginController'
+		});
+})
--- app/components/controllers/loginController/loginController.js
+++ app/components/controllers/loginController/loginController.js
@@ -1,28 +1,36 @@
 
-app.factory("authenticationSvc", function($http, $q, $window) {
-  var userInfo;
 
-  function login(userName, password) {
-    var deferred = $q.defer();
+app.controller('LoginController', LoginController);
 
-    $http.post("/api/login", {
-      userName: userName,
-      password: password
-    }).then(function(result) {
-      userInfo = {
-        accessToken: result.data.access_token,
-        userName: result.data.userName
-      };
-      $window.sessionStorage["userInfo"] = JSON.stringify(userInfo);
-      deferred.resolve(userInfo);
-    }, function(error) {
-      deferred.reject(error);
-    });
+function LoginController($scope){
+  $scope.test = 'Hey You From Login'
+}
 
-    return deferred.promise;
-  }
 
-  return {
-    login: login
-  };
-});
+// app.factory("authenticationSvc", function($http, $q, $window) {
+//   var userInfo;
+
+//   function login(userName, password) {
+//     var deferred = $q.defer();
+
+//     $http.post("/api/login", {
+//       userName: userName,
+//       password: password
+//     }).then(function(result) {
+//       userInfo = {
+//         accessToken: result.data.access_token,
+//         userName: result.data.userName
+//       };
+//       $window.sessionStorage["userInfo"] = JSON.stringify(userInfo);
+//       deferred.resolve(userInfo);
+//     }, function(error) {
+//       deferred.reject(error);
+//     });
+
+//     return deferred.promise;
+//   }
+
+//   return {
+//     login: login
+//   };
+// });
