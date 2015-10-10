dfb7816996c2313b18a07121c7c4fd88a8c2d9a9
auron1223 <marcuslyons@u.boisestate.edu>
2015-10-10 06:56:28 -0600

    Added some changes to the userService.js, and other user files.

--- app/components/controllers/loginController/loginController.js
+++ app/components/controllers/loginController/loginController.js
@@ -1,17 +1,34 @@
-app.controller('LoginController', LoginController);
+var app = angular.module('firebaseLogin');
 
-function LoginController($scope, $location){
-  $scope.test = 'Hey You From Login'
-  
-  $scope.login = function(){
-    if($scope.user.name && $scope.user.password){
-      // AuthService.login($scope.user).then(function(err, authData){
-      //   if(err){
-      //    return $scope.error = err;
-      //   }
-      //   $location.path('dashboard');
-      // })
+app.controller('LoginCtrl', function ($scope, authService, $location) {
+
+  $scope.login = function () {
+    return authService.login($scope.details, function (user) {
+      user.uid = user.uid.replace('simplelogin:', '');
+      $scope.$apply(function () {
+        $location.path('/dashboard/' + user.uid)
+      });
+    });
+  };
+
+  $scope.register = function () {
+    return authService.register($scope.details, function (user) {
+      user.uid = user.uid.replace('simplelogin:', '');
+      $scope.$apply(function () {
+        $location.path('/dashboard/' + user.uid)
+      });
+    });
+  };
+
+  $scope.status = 'Register';
+  $scope.showReg = function () {
+    if ($scope.status === 'Register') {
+      $scope.status = 'Login';
+    } else {
+      $scope.status = 'Register';
     }
-  }
-  
-}
+    $scope.reg = !$scope.reg;
+  };
+
+
+});
